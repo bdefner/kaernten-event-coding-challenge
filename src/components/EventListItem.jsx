472761dbe1b5584334aea1d99ea3e7f04b435074
Beppino Defner <hallo@deona.at>
2023-01-29 16:59:29 +0100

    Fix problem with <Link /> component

--- src/components/EventListItem.jsx
+++ src/components/EventListItem.jsx
@@ -1,7 +1,44 @@
+import moment from 'moment-timezone';
 import { useState } from 'react';
 import iconCalendar from '../../public/icons/calendar.png';
 import iconPin from '../../public/icons/pin.png';
 import Button from './Button';
+
+function handleIcsDownload(props) {
+  // Convert the date strings to valid timezone identifiers
+  const startDate = moment
+    .tz(props.startDate, 'YYYY-MM-DDTHH:mm:ss.SSSZ', 'UTC')
+    .format('YYYYMMDDTHHmmss');
+  const endDate = moment
+    .tz(props.endDate, 'YYYY-MM-DDTHH:mm:ss.SSSZ', 'UTC')
+    .format('YYYYMMDDTHHmmss');
+  const timezone = 'Europe/Vienna';
+
+  // Create the .ics file content
+  const icsData = `BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:${props.title}
+DTSTART;TZID=${timezone}:${startDate}
+DTEND;TZID=${timezone}:${endDate}
+LOCATION:${props.location}
+END:VEVENT
+END:VCALENDAR`;
+
+  // Create a Blob object with the .ics file content
+  const blob = new Blob([icsData], { type: 'text/calendar' });
+
+  // Create a URL for the Blob object
+  const url = URL.createObjectURL(blob);
+
+  // Create an a element
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = `${props.title}.ics`;
+
+  // Simulate a click on the a element to trigger the download
+  a.click();
+}
 
 function convertDateString(dateString) {
   const date = new Date(dateString);
@@ -43,20 +80,29 @@ export default function EventListItem(props) {
             />
           </div>
           {!isCollapsed && (
-            <Button
-              type="iconLink"
-              iconType="download"
-              span={''}
-              text={'in meinen Kalender speichern'}
-            />
+            <div
+              onClick={() => {
+                handleIcsDownload(props);
+              }}
+            >
+              <Button
+                type="iconLink"
+                iconType="download"
+                span={''}
+                text={'in meinen Kalender speichern'}
+              />
+            </div>
           )}
+          {/* onClick={() => history.push(`/event/${props.title}`)} */}
           {!isCollapsed && (
-            <Button
-              type="iconLink"
-              iconType="open"
-              span={''}
-              text={'als Seite öffnen'}
-            />
+            <div>
+              <Button
+                type="iconLink"
+                iconType="open"
+                span={''}
+                text={'als Seite öffnen'}
+              />
+            </div>
           )}
         </div>
 
