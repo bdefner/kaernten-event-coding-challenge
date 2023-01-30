a7f2d4c506cddd9dd371f70d1895bf9591a6358c
Beppino Defner <hallo@deona.at>
2023-01-30 16:02:11 +0100

    Edit vide.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import rollupReplace from "@rollup/plugin-replace";
+import rollupReplace from '@rollup/plugin-replace';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,26 +9,27 @@ export default defineConfig({
     rollupReplace({
       preventAssignment: true,
       values: {
-        __DEV__: JSON.stringify(true),
-        "process.env.NODE_ENV": JSON.stringify("development"),
+        DEV: JSON.stringify(true),
+        'process.env.NODE_ENV': JSON.stringify('development'),
       },
     }),
     react(),
   ],
+  outDir: './build',
   resolve: process.env.USE_SOURCE
     ? {
         alias: {
-          "@remix-run/router": path.resolve(
+          '@remix-run/router': path.resolve(
             __dirname,
-            "../../packages/router/index.ts"
+            '../../packages/router/index.ts',
           ),
-          "react-router": path.resolve(
+          'react-router': path.resolve(
             __dirname,
-            "../../packages/react-router/index.ts"
+            '../../packages/react-router/index.ts',
           ),
-          "react-router-dom": path.resolve(
+          'react-router-dom': path.resolve(
             __dirname,
-            "../../packages/react-router-dom/index.tsx"
+            '../../packages/react-router-dom/index.tsx',
           ),
         },
       }
